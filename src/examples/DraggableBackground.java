ff384e3b0ac29eff0f4bf21a4cbac141847fe114
skot <baronvasdeferens@gmail.com>
2017-06-02 23:00:45 -0700

    examples folder and content

---
+++ src/examples/DraggableBackground.java
@@ -0,0 +1,205 @@
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class DraggableBackground extends Tile implements GloballyScalable {
+
+    // Renders sprites from low layer number(0) to high (9)
+    ArrayList<PictureTile>[] spritesByLayer;
+
+    PictureTile dragTarget = null;
+
+    private int priorX = 0;                     // prior locations of mouse pointer (for scrolling)
+    private int priorY = 0;
+
+    BigDecimal scaleFactor;
+
+    boolean displayCoords = false;
+
+
+    DraggableBackground(int width, int height) {
+        super(width, height, 0, Color.WHITE);
+
+        spritesByLayer = new ArrayList[10];
+        for (int i = 0; i < 10; i++) {
+            spritesByLayer[i] = new ArrayList<>();
+        }
+
+        scaleFactor = new BigDecimal(1.0f).setScale(2);
+
+    }
+
+    /**
+     * Adds sprites to the correct layer.
+     */
+    public void addSprite(PictureTile pTile, int layer) {
+        spritesByLayer[layer].add(pTile);
+    }
+
+
+    /**
+     * Darws sprites from low to high
+     *
+     * @param graphics
+     */
+    public void updateAndPaint(Graphics graphics) {
+        Arrays.stream(spritesByLayer)
+                .forEach((list) -> list.forEach(sprite -> sprite.updateAndDrawGraphics(graphics)));
+    }
+
+    @Override
+    public void updateAndDrawGraphics(Graphics g) {
+
+        updateAndPaint(g);
+
+        if (displayCoords) {
+            g.setColor(Color.RED);
+            PointerInfo pointerInfo = MouseInfo.getPointerInfo();
+            g.drawString("x: " + pointerInfo.getLocation().getX() +
+                    " y: " + pointerInfo.getLocation().getY(), 10, 20);
+        }
+
+    }
+
+
+    /**
+     * Finds the Sprite with the highest "layer" attribute that contains the click
+     *
+     * @param me
+     * @return
+     */
+    public Clickable assessClick(MouseEvent me) {
+
+        Point p = me.getPoint();
+        PictureTile assumedTarget = null;
+
+        for (ArrayList<PictureTile> list : spritesByLayer) {
+
+            for (PictureTile pTile : list) {
+
+                if (pTile.isBackground())
+                    assumedTarget = pTile;
+                else if (pTile.containsPoint(p)) {
+                    assumedTarget = pTile;
+                }
+            }
+        }
+
+        if (assumedTarget == null)
+            return null;
+        else
+            dragTarget = assumedTarget;
+
+        priorX = me.getX();
+        priorY = me.getY();
+
+        return dragTarget;
+    }
+
+    /**
+     * Updates the positions of the various Sprites
+     *
+     * @param e
+     */
+    public void moveTarget(MouseEvent e) {
+
+        if (dragTarget != null) {
+
+            if (dragTarget.isBackground()) {
+
+                int xDelta = priorX - e.getX();
+                priorX = e.getX();
+                dragTarget.applyX(-xDelta);
+
+                int yDelta = priorY - e.getY();
+                priorY = e.getY();
+                dragTarget.applyY(-yDelta);
+
+                // Update all other entities, skipping layer 0
+                for (int i = 1; i < 10; i++) {
+                    for (PictureTile pTile : spritesByLayer[i]) {
+                        pTile.applyX(-xDelta);
+                        pTile.applyY(-yDelta);
+                    }
+                }
+            } else {
+                int xDelta = priorX - e.getX();
+                priorX = e.getX();
+                dragTarget.applyX(-xDelta);
+
+                int yDelta = priorY - e.getY();
+                priorY = e.getY();
+                dragTarget.applyY(-yDelta);
+            }
+
+        }
+    }
+
+    /**
+     * Called when the user releases the mouse
+     *
+     * @param e
+     */
+    public void releaseTarget(MouseEvent e) {
+
+        priorX = e.getX();
+        priorY = e.getY();
+        dragTarget = null;
+
+        for (ArrayList<PictureTile> list : spritesByLayer) {
+            for (Sprite sprite : list) {
+                if (sprite instanceof PictureTile) {
+                    PictureTile picTile = (PictureTile) sprite;
+                    picTile.updatePolygon();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void scaleUp(MouseEvent e) {
+
+        scaleFactor = scaleFactor.add(scaleStep).setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+        if (scaleFactor.compareTo(BigDecimal.ONE) == 1)
+            scaleFactor = BigDecimal.ONE;
+
+        priorX = e.getX();
+        priorY = e.getY();
+
+        for (ArrayList<PictureTile> list : spritesByLayer) {
+            for (Sprite sprite : list) {
+                if (sprite instanceof GloballyScalable) {
+                    GloballyScalable globScale = (GloballyScalable) sprite;
+                    globScale.scaleUp(e);
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void scaleDown(MouseEvent e) {
+
+        scaleFactor = scaleFactor.subtract(scaleStep).setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+        if (scaleFactor.compareTo(new BigDecimal(0.1f)) <= 0)
+            scaleFactor = new BigDecimal(0.1f);
+
+        priorX = e.getX();
+        priorY = e.getY();
+
+        for (ArrayList<PictureTile> list : spritesByLayer) {
+            for (Sprite sprite : list) {
+                if (sprite instanceof GloballyScalable) {
+                    GloballyScalable globScale = (GloballyScalable) sprite;
+                    globScale.scaleDown(e);
+                }
+            }
+        }
+    }
+}
---
+++ src/examples/ScatterTile.java
@@ -0,0 +1,88 @@
+import java.awt.*;
+
+/**
+ * Created by skot on 3/31/17.
+ */
+public class ScatterTile extends Tile {
+
+    private int boogieFactorX = 0, boogieFactorY = 0;
+    private int lowerBound, upperBound, leftBound, rightBound;
+
+    public ScatterTile(int x, int y, int size, Color backgroundColor) {
+
+        super(x,y,size,backgroundColor);
+        resetBoogieFactors();
+    }
+
+
+    public void setBounds(int left, int right, int top, int bottom) {
+        this.upperBound = top;
+        this.lowerBound = bottom;
+        this.leftBound = left;
+        this.rightBound = right;
+    }
+
+    public boolean checkBounds() {
+        return (x < leftBound) || (x > rightBound) || (y < upperBound) || (y > lowerBound);
+    }
+
+
+    @Override
+    public synchronized void updateAndDrawGraphics(Graphics g) {
+
+        x += boogieFactorX;
+        y += boogieFactorY;
+
+//        changeColor();
+
+        if (checkBounds()) {
+            resetBoogieFactors();
+        }
+
+        g.setColor(Color.RED);
+        g.fillRect(x, y, size, size);
+    }
+
+    public boolean containsPoint(Point p) {
+
+        if ((p.getX() >= x) && (p.getX() <= x + size)){
+            if ((p.getY() >= y) && (p.getY() <= y + size)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void resetBoogieFactors() {
+
+        boogieFactorX = rando.nextInt(8) - rando.nextInt(7);
+        boogieFactorY = rando.nextInt(8) - rando.nextInt(7);
+
+        if  (boogieFactorX == 0 && boogieFactorY == 0) {
+
+            while (boogieFactorX == 0) {
+                boogieFactorX = rando.nextInt(8) - rando.nextInt(7);
+            }
+            while (boogieFactorY == 0) {
+                boogieFactorY = rando.nextInt(8) - rando.nextInt(7);
+            }
+        }
+    }
+
+    public void changeColor() {
+//
+//        int red = color.getRed();
+//        int green = color.getGreen();
+//        int blue = color.getBlue();
+//
+//        red = red - 1;
+//        blue = blue - 2;
+//
+//        if (red <= 0) {
+//            red = 0;
+//            disposeOnNextUpdate = true;
+//        }
+
+        //color = new Color(255, 0, 0);
+    }
+}
